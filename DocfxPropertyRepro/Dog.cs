1a3da28f2597106ed62aeabfbd69065dc7ae996e
Mark Waterman <markwaterman@users.noreply.github.com>
2024-01-18 16:07:26 -0800

    Add project files.

---
+++ DocfxPropertyRepro/Dog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DocfxPropertyRepro
+{
+    /// <summary>
+    /// Class representing a dog.
+    /// </summary>
+    public class Dog
+    {
+        /// <summary>
+        /// Name of the dog.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Age of the dog.
+        /// </summary>
+        public int Age { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name">Name of the dog.</param>
+        /// <param name="age">Age of the dog.</param>
+        public Dog(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
